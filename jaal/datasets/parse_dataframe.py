076572a8bbb70bc46710ef6b30b17ea64c3cf9d8
imohitmayank <mohitmayank1@gmail.com>
2021-02-03 01:01:43 +0530

    converting node ids in node_df and edge_df to string

--- jaal/datasets/parse_dataframe.py
+++ jaal/datasets/parse_dataframe.py
@@ -22,18 +22,26 @@ def parse_dataframe(edge_df, node_df=None):
     if node_df is not None:
         if 'id' not in node_df.columns:
             raise Exception("Node dataframe missing 'id' column.")
-    
+
+    # Data post processing - convert the from and to columns in edge data as string for searching
+    edge_df.loc[:, ['from', 'to']] = edge_df.loc[:, ['from', 'to']].astype(str)
+
     # create node list w.r.t. the presence of absence of node_df
     nodes = []
     if node_df is None:
         node_list = list(set(edge_df['from'].unique().tolist() + edge_df['to'].unique().tolist()))
         nodes = [{'id': node_name, 'label': node_name, 'shape': 'dot', 'size': 7} for node_name in node_list]
     else:
+        # convert the node id column to string
+        node_df.loc[:, 'id'] = node_df.loc[:, 'id'].astype(str)
+        # create the node data
         for node in node_df.to_dict(orient='records'):
             nodes.append({**node, **{'label': node['id'], 'shape': 'dot', 'size': 7}})
+    
     # create edges from df
     edges = []
     for row in edge_df.to_dict(orient='records'):
-        edges.append({**row, **{'id': str(row['from']) + "__" + str(row['to']), 'color': {'color': '#97C2FC'}}})
+        edges.append({**row, **{'id': row['from'] + "__" + row['to'], 'color': {'color': '#97C2FC'}}})
+    
     # return
     return {'nodes': nodes, 'edges': edges}

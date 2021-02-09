829e76e2ace2cb0fda87b010357e36b5c005c523
imohitmayank <mohitmayank1@gmail.com>
2021-02-10 01:30:38 +0530

    v0.0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="jaal",
-    version="0.0.5",
+    version="0.0.6",
     author="Mohit Mayank",
     author_email="mohitmayank1@gmail.com",
     description="jaal - your interactive network visualizer dashboard",

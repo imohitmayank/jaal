2b1e4696ca0d3d8f074827e5ae2943817eaa88e7
Mohit <mohitmayank1@gmail.com>
2021-05-23 15:12:57 +0530

    bumped to v0.0.8

--- jaal_call.py
+++ jaal_call.py
@@ -1,7 +1,17 @@
 # import
 from jaal import Jaal
 from jaal.datasets import load_got
+
 # load the data
 edge_df, node_df = load_got()
-# init Jaal and run server
-Jaal(edge_df, node_df).plot(debug=True)
+
+# define vis options
+vis_opts = {'height': '600px', # change height
+            'interaction':{'hover': True}, # turn on-off the hover 
+            'physics':{'stabilization':{'iterations': 100}}} # define the convergence iteration of network
+
+# init Jaal and run server (with opts)
+Jaal(edge_df, node_df).plot(vis_opts=vis_opts)
+
+# init Jaal and run server (with default options)
+# Jaal(edge_df, node_df).plot()
